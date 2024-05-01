456cbdac28a967298729a61860dab7501003c53a
salim laimeche <slaimeche@webexpr.fr>
2024-05-01 02:07:44 +0200

    Add tfjs-vis library for data visualization

--- app/chapter1/page.tsx
+++ app/chapter1/page.tsx
@@ -1,44 +1,84 @@
 "use client";
 
 import * as tf from '@tensorflow/tfjs'
-import { Button } from 'antd';
+import * as tfvis from '@tensorflow/tfjs-vis'
+import { Button, Spin, Table } from 'antd';
 import { useState } from 'react'
+import { generateColumns } from '../utils/utils';
 
 export default function Chapter1(){
 
-    const [data, setData] = useState<tf.data.CSVDataset>()
+    const [data, setData] = useState<tf.TensorContainer[]>()
+    const [display, setDisplay] = useState<any>()
+    const [columnsName, setColumnsName] = useState<string[]>()
+    const [loading, setLoading] = useState<boolean>(false)
+    const visor = tfvis.visor();
+    visor.el.style.color = 'black';
 
     const csvUrl =
     'https://storage.googleapis.com/tfjs-examples/multivariate-linear-regression/data/boston-housing-train.csv';
     
-    async function run() {
-      // We want to predict the column "medv", which represents a median value of
-   // a home (in $1000s), so we mark it as a label.
-   const csvDataset = tf.data.csv(
-    csvUrl, {
-      columnConfigs: {
-        medv: {
-          isLabel: true
+    async function run(){
+    setLoading(true)
+    const csvDataset = tf.data.csv(csvUrl)
+    const names =  await csvDataset.columnNames()
+    setColumnsName(names)
+    const dataToDispalay = await csvDataset.toArray()
+    setData(dataToDispalay)
+    const name = 'Data from CSV'
+    tfvis.render.linechart({name,tab:"LineChart"}, {values: dataToDispalay.map((d: any) => ({x: d.lstat, y: d.medv}))}, {xLabel: 'LSTAT', yLabel: 'MEDV', height: 300})
+    tfvis.render.scatterplot({name, tab:"ScatterPlot"}, {values: dataToDispalay.map((d: any) => ({x: d.lstat, y: d.medv}))}, {xLabel: 'LSTAT', yLabel: 'MEDV', height: 300})
+    setLoading(false)
+  }
+
+  async function createModel(){
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [12]}));
+    model.compile({loss: 'meanSquaredError', optimizer: 'adam'})
+    const surfaceModel = {name: 'Model Summary', tab: 'Model'}
+    tfvis.show.modelSummary(surfaceModel, model)
+    const csvDataset = tf.data.csv(
+      csvUrl, {
+        columnConfigs: {
+          medv: {
+            isLabel: true
+          },
+          zn:{
+            required:false
+          },
+          indus:{
+            required:false
+          },
+
+
         }
-      }
-    });
+      });
 
-  // Number of features is the number of column names minus one for the label
-  // column.
-const numOfFeatures = (await csvDataset.columnNames()).length - 1;
+      const history = await model.fitDataset(csvDataset, {
+        epochs: 4,
+        callbacks: {
+          onEpochEnd: (epoch, logs) => console.log(logs.loss)
+        }
+      });
+  }
 
-// Prepare the Dataset for training
-const flattenedDataset = csvDataset.map(() => {
 
-})
-}
+  if(loading) return <Spin fullscreen={true} tip="Loading"/>
 
 return (
     <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-6xl font-bold">RNN</h1>
+      {data ? (
+        <>
+        <Button type='primary' onClick={() => visor.toggle()} className='m-3'>Toggle Visor</Button>
+        <Button type='primary' onClick={createModel} className='m-3'>Create Model</Button>
+        <Table dataSource={data} columns={generateColumns(data)} />
+        </>
+      ):(
         <Button type='primary' onClick={run}>
-            Load Data
-        </Button>
+        Load Boston Housing Dataset
+    </Button>
+      )}
     </div>
-)
-}
+      )
+      }
+  
---
+++ app/utils/utils.tsx
@@ -0,0 +1,56 @@
+import { Button } from "antd";
+import { ColumnGroupType, ColumnType } from "antd/es/table";
+
+
+const checkEveryField = (item: any, filter: string): boolean => {
+    try {
+      return Object.keys(item).some((key) => {
+        if (typeof item[key] === "string" && item[key].toLowerCase().includes(filter.toLowerCase())) 
+        return true;
+        if (typeof item[key] === "number" && item[key].toString().toLowerCase().includes(filter.toLowerCase())) return true;
+        if (item[key] instanceof Date && item[key].toString().toLowerCase().includes(filter.toLowerCase())) return true;
+        if (typeof item[key] === "object" && item[key] !== null) {
+          return checkEveryField(item[key], filter);
+        }
+        return false;
+      });
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+  
+  export const filteredList = (list: any[], filter: string) => {
+    try {
+      return list.filter((item) => checkEveryField(item, filter));
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  type CustomColumnType<T> = ColumnType<T> | ColumnGroupType<T>;
+  export const generateColumns = (data:any): CustomColumnType<any>[] => {
+        const columns: CustomColumnType<any>[]= [];
+        if (data && data.length > 0 && data[0]) {
+          let maxKeysObj = data[0];
+          let maxKeys = Object.keys(data[0]).length;
+        
+          data.forEach((obj: {}) => {
+            const numKeys = Object.keys(obj).length;
+            if (numKeys > maxKeys) {
+            maxKeys = numKeys;
+            maxKeysObj = obj;
+            }
+          });
+          const headers = Object.keys(maxKeysObj);
+          headers.forEach((header) => {
+            columns.push({
+              title: header,
+              dataIndex: header,
+              key: header,
+            });
+          });
+        }
+        return columns;
+      }
