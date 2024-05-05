f98cd9b8112709cd06411b717b883cbbf1cf09c4
salim laimeche <slaimeche@webexpr.fr>
2024-05-05 14:58:32 +0200

    Refactor build script to move generated files to dist directory

--- bin/build.sh
+++ bin/build.sh
@@ -9,11 +9,3 @@ mkdir -p dist
 # Exécute la construction de ton projet (remplace cette commande par celle de ton projet)
 # Assure-toi que cette commande génère les fichiers dans le répertoire "dist"
 npm run build
-
-# Vérifie si des fichiers sont présents dans le répertoire "dist"
-if [ -n "$(ls -A dist)" ]; then
-  # Déplace les fichiers générés dans le répertoire "dist"
-  mv dist/* ./dist/
-else
-  echo "Aucun fichier à déplacer dans le répertoire 'dist'."
-fi
