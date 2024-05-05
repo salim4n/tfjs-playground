d2457f3f19af6ada62de22cf659566539e63c777
salim laimeche <slaimeche@webexpr.fr>
2024-05-05 14:49:42 +0200

    Update import in page.tsx

--- app/chapter1/page.tsx
+++ app/chapter1/page.tsx
@@ -2,7 +2,7 @@
 
 import * as tf from '@tensorflow/tfjs'
 import * as tfvis from '@tensorflow/tfjs-vis'
-import { Button, Empty, Spin, Table } from 'antd';
+import { Button, Empty, Popover, Spin, Table } from 'antd';
 import {  useEffect, useState } from 'react'
 import { generateColumns, normalizeData, underConstruct } from '../utils/utils';
 import { drawBarChartOfEachFeatureOfOneDataset, drawHeatMap, drawHistogram, drawLine, drawScatterPlot, drawTable } from '../utils/visor/draw';
@@ -46,45 +46,6 @@ export default function Chapter1(){
     setLoading(false)
   }
 
-  async function createModel() {
-    const csvDataset = tf.data.csv(csvUrl, {
-      columnConfigs: {
-        medv: {
-          isLabel: true
-        }
-      },
-      columnNames: columnsName, // specify the column names
-      
-    });
-  
-    const numFeatures = columnsName.length - 1; // Excluding the label column
-  
-    console.log('Nombre de caractéristiques:', numFeatures);
-  
-    // Display a sample of the data to verify the structure
-    csvDataset.take(1).forEachAsync(sample => console.log('Échantillon de données:', sample));
-  
-    const model = tf.sequential();
-    model.add(tf.layers.dense({
-      units: 64,
-      inputShape: [numFeatures],
-      activation:'relu',
-      dtype: 'float32',
-      kernelInitializer: 'leCunNormal'
-    }));
-    model.add(tf.layers.dense({ units: 1 }));
-    model.compile({ loss:'meanSquaredError', optimizer: 'adam' });
-  
-    tfvis.show.modelSummary({ name: 'Model Summary', tab: 'Model' }, model);
-    tfvis.show.layer({ name: 'Input', tab: 'Model' }, model);
-  
-    const history = await model.fitDataset(csvDataset, {
-      epochs: 4,
-      callbacks: {
-        onEpochEnd: (epoch, logs) => console.log(`Epoch ${epoch + 1}, Loss: ${logs.loss}`)
-      }
-    });
-  }
 
 if(loading) return <Spin fullscreen={true} tip="Loading"/>
 
@@ -93,7 +54,9 @@ return (
       {data ? (
         <>
         <Button type='primary' onClick={() => visor.toggle()} className='m-3'>Toggle Visor</Button>
-        <Button type='primary' onClick={createModel} className='m-3'>Create Model</Button>
+        <Popover content="Model not defined actually...">
+        <Button type='primary' disabled className='m-3'>Create Model</Button>
+        </Popover>
         <Table dataSource={data} columns={generateColumns(data)} />
         </>
       ):(
